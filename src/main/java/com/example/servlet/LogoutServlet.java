461a829ceb9e1ef86cebf4ed93a45c8b31549f9d
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-02-25 17:54:50 +0500

    first

--- src/main/java/com/example/servlet/LogoutServlet.java
+++ src/main/java/com/example/servlet/LogoutServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 @WebServlet("/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession(false);
 
         if (session!=null){
